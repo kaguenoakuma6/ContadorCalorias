da3f062c47c730ae43cec923e13b7a7f5e0ae6d9
Kaguenoakuma <shadowfocker666@gmail.com>
2025-01-05 00:13:53 -0600

    Proyecto terminado

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react"
+import { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay";
+
+type CalorieTrackerProps = {
+    activities: Activity[]
+}
+
+export default function CalorieTracker( {activities} : CalorieTrackerProps ) {
+
+    const caloriesConusumed = useMemo( () => activities.reduce( (total, activity) => activity.category === 1 ? total + activity.calories : total, 0) , [activities] );
+    const caloriesBourned = useMemo( () => activities.reduce( (total, activity) => activity.category === 2 ? total + activity.calories : total, 0) , [activities] );
+    const netCalories = useMemo( () => caloriesConusumed - caloriesBourned , [activities]);
+
+  return (
+    <>
+        <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
+        <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+            <CalorieDisplay
+                calories={caloriesConusumed}
+                text="Consumidas"
+            />
+            <CalorieDisplay
+                calories={caloriesBourned}
+                text="Quemadas"
+            />
+            <CalorieDisplay
+                calories={netCalories}
+                text="Diferencia"
+            />
+
+        </div>
+    </>
+  )
+}
